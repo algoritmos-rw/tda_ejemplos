b50406bd98f0bdd2ace540dd4bce6fa781befb77
Martín Buchwald <martin.buchwald@mulesoft.com>
2023-07-18 11:22:02 -0300

    Problemas de PL

---
+++ pl/ejemplo0.py
@@ -0,0 +1,18 @@
+import pulp
+
+
+def ejemplo():
+    x = pulp.LpVariable("x")
+    y = pulp.LpVariable("y")
+    problem = pulp.LpProblem("products", pulp.LpMaximize)
+    problem += 3 * x - y <= 0
+    problem += x + 2 * y <= 14
+    problem += x - y <= 2
+    problem += 5 * x + 3 * y
+    problem.solve()
+    return pulp.value(x), pulp.value(y)
+
+
+if __name__ == "__main__":
+    x, y = ejemplo()
+    print(x, y)
---
+++ pl/mochila.py
@@ -0,0 +1,36 @@
+from typing import List
+
+import pulp
+
+
+def cargar_mochila(ruta):
+    w = []
+    v = []
+    with open(ruta) as f:
+        W = int(f.readline().strip())
+        for l in f:
+            vi, wi = l.strip().split(",")
+            w.append(int(wi))
+            v.append(int(vi))
+    return v, w, W
+
+
+def mochila_variable(v: List[int], w: List[int], W: int):
+    y = []
+    for i in range(len(v)):
+        y.append(pulp.LpVariable("y" + str(i), cat="Binary"))
+
+    problem = pulp.LpProblem("products", pulp.LpMaximize)
+    problem += pulp.LpAffineExpression([(y[i], w[i]) for i in range(len(y))]) <= W
+    problem += pulp.LpAffineExpression([(y[i], v[i]) for i in range(len(y))])
+
+    problem.solve()
+    return list(map(lambda yi: pulp.value(yi), y))
+
+
+if __name__ == "__main__":
+    v, w, W = cargar_mochila("mochila.txt")
+    y = mochila_variable(v, w, W)
+    print(y)
+    print("Peso usado:", sum([w[i] * y[i] for i in range(len(y))]))
+    print("Valor obtenido:", sum([v[i] * y[i] for i in range(len(y))]))
